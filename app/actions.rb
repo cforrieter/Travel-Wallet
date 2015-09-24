a6a8b6b1a4c1ebdff12384aece2fd6dd450aac9b
Mary Lee <mango.gal@gmail.com>
2015-09-24 02:25:04 +0000

    adds some routes

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,22 @@
 get '/' do
   erb :index
 end
+
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  email = params[:email]
+  password = params[:password]
+
+  user = User.find_by(email: email, password: password)
+
+  if user
+    session[:user_id] = user.user_id
+  else
+    session[:error] = "Your log in information is incorrect"
+    redirect '/login'
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, length: {in: 6..100}
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :password, presence: true, length: {minimum: 6}
+  validates :login, presence: true
+
+  before_validation :ensure_login_has_a_value
+
+  protected
+    def ensure_login_has_a_value
+      if login.nil?
+        self.login = email unless email.blank?
+      end
+    end
 end
