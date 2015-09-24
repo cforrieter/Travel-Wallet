007ed4ebfe3d72601685619ed7a39c16364b6691
Mary Lee <mango.gal@gmail.com>
2015-09-24 01:44:00 +0000

    creates validations for models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :categories
   has_many :shares
+
+  validates :first_name, presence: true, length: {minimum: 2}
+  validates :last_name, presence: true, length: {minimum: 2}
+  validates :email, presence: true, uniqueness: true, length: {in: 6..100}
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  validates :password, presence: true, length: {minimum: 6}
 end
