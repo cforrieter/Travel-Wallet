318f991a08de0149b14695eea589a49f36ef2bb3
cforrieter <cforrieter@gmail.com>
2015-09-26 02:56:58 +0000

    Added uniqueness to user's shares

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,11 @@ class Category < ActiveRecord::Base
   has_many :shares
   has_many :documents
 
+  def hash
+    self.user_id
+  end
+
+  def eql?(other_obj)
+    self.user_id == other_obj.user_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,7 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :password_hash, presence: true, length: {minimum: 6}
 
+  def connections
+    self.shares.map { |share| share.category }.uniq
+  end
 end
